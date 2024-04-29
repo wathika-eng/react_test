5260c57fd92c79e8fe3b75e44b106b65c702192f
jwathika <wathika02@gmail.com>
2024-04-29 10:22:37 +0300

    Update Header component to display additional information

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 const key = process.env.REACT_APP_MY_KEY;
-console.log(process.env);
+const port = process.env.REACT_APP_MY_SECOND_KEY;
+const pass = process.env.REACT_APP_MY_PASS;
 const Header = () => {
 	return (
 		<>
-			<p>I am using {key}</p>
+			<p>
+				I am using my key {key} on port {port}
+			</p>
+			<p>My password is {pass}</p>
 		</>
 	);
 };
